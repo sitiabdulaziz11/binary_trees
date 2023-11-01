f7442e31cfdb5d7de189a0f6298b3e37f1d3effe
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-11-01 11:14:07 +0000

    updared righ and left insert

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -27,9 +27,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent->left != NULL)
 	{
-		temp = parent->left;
-		parent->left = new_node;
-		new_node->left = temp;
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
 	}
 	parent->left = new_node;
 
